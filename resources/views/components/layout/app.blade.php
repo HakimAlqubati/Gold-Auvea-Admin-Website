a25797690d47d2f2dd79bed7f94886387e0fa6bc
HakimAlqubati <hakimahmed123321@gmail.com>
2025-11-23 20:23:58 +0300

    add font family

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Auvea 3D Studio – Gold & Jewelry Design in Yemen</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/auvea/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/auvea/cart.css') }}">
 </head>
